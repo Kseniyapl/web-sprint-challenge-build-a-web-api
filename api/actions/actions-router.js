9cf2fc805a56607e01f774f1f4e3ea6ba9ecab05
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-12-03 17:25:40 -0500

    add get actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,17 @@
-// Write your "actions" router here!
+const Action = require('../actions/actions-model')
+const express = require('express');
+
+
+const router = express.Router();
+
+router.get('/', (req, res, next)=>{
+    Action.get()
+    .then(actions=>{
+        res.status(200).json(actions)
+    })
+    .catch(error=>{
+        next(error)
+    })
+})
+
+module.exports = router;
