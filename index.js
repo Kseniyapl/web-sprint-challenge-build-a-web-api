accc5e3a5599013a4adee57ee31431d5dc2358c2
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-12-03 13:27:03 -0500

    wip

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ Pull your server into this file and start it!
 
 const server = require('./api/server.js');
 
-const port = 2020;
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
